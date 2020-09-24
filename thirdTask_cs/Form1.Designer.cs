b5362ec8f2e19faf219990538a26d4b8a1d3c47f
Sashka <osninogi@gmail.com>
2020-09-24 21:47:41 +0300

    а

---
+++ thirdTask_cs/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace thirdTask_cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Понедельник",
+            "Вторник",
+            "Среда",
+            "Четверг",
+            "Пятница",
+            "Суббота",
+            "Воскресенье"});
+            this.listBox2.Location = new System.Drawing.Point(16, 59);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(200, 95);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.ListBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Узнать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(500, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Выберите день и введите дату, чтобы узнать какая сейчас пара или идет перемена.";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(16, 160);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.Value = new System.DateTime(2020, 2, 2, 20, 17, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.DateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(523, 251);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Name = "Form1";
+            this.Text = "Расписание";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ thirdTask_cs/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace thirdTask_cs
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Int32[] usual_days = { 0, 1, 2, 4 };
+        static DateTime beginning = new DateTime(2020, 2, 2, 8, 0, 0);
+        static Boolean isLesson = true;
+        private static DateTime add_lesson_time(DateTime time)
+        {
+            time = time.AddHours(1);
+            time = time.AddMinutes(20);
+            return time;
+        }
+        private static String Get_Current_Lesson(DateTime time, Int32 time_for_break = 15)
+        {
+            Int32 lesson = 0;
+            String result = "";
+            for (DateTime beginning = new DateTime(2020, 2, 2, 8, 0, 0); beginning < time; lesson++)
+            {
+                beginning = add_lesson_time(beginning);
+                if (beginning < time)
+                {
+                    beginning = beginning.AddMinutes(time_for_break);
+                    isLesson = beginning > time ? false : true;
+                    lesson = beginning == time ? lesson + 1 : lesson;
+                }
+                if (lesson > 6)
+                {
+                    break;
+                }
+            }
+            if (lesson > 6)
+            {
+                result = "Пары закончились";
+            }
+            else if (!isLesson)
+            {
+                result = "Сейчас идёт перемена.";
+            }
+            else if (beginning > time)
+            {
+                result = "Пары еще не начались";
+            }
+            else
+            {
+                result = String.Format("Сейчас идет {0} пара", lesson);
+            }
+            return result;
+        }
+
+        private void ListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+            {
+                int fl; fl = listBox2.SelectedIndex;
+                DateTime time = dateTimePicker1.Value;
+
+                fl = usual_days.Contains(fl) ? -2 : fl; // -2 для обычных дней
+                switch (fl)
+                {
+                    case (-2):
+                        {
+                            MessageBox.Show(Get_Current_Lesson(time));
+                            break;
+                        }
+                    case (-1):
+                        {
+                            MessageBox.Show("Вы не выбрали день");
+                            break;
+                        }
+                    case (3):
+                        {
+                            MessageBox.Show("Сегодндя физ-ра");
+                            break;
+                        }
+                    case (5):
+                        {
+                            MessageBox.Show(Get_Current_Lesson(time, time_for_break: 10));
+                            break;
+                        }
+                    case (6):
+                        {
+                            MessageBox.Show("Сегодня выходной");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
